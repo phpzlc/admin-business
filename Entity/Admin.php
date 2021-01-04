0d0cba2ba33a110a1d73f837c8b423f5bcac8284
exodusht <1847944340@qq.com>
2021-01-04 09:41:29 +0800

    fix:修改UserInterface引用位置

--- Entity/Admin.php
+++ Entity/Admin.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\AdminRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use App\Business\AuthBusiness\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=AdminRepository::class)
